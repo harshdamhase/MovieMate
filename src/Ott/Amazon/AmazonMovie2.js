5f9e37343af0e188ef90797b41527d8ba19760e0
Mr-Siddhant-Bagul <siddhantbagul16@gmail.com>
2023-05-13 18:17:34 +0530

    New Files Added

---
+++ src/Ott/Amazon/AmazonMovie2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AmazonMovie2() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AmazonMovie2
---
+++ src/Ott/Netflix/NetflixMovie1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./Netflix.css"
+import NavbarComp from '../../Components/NavbarComp'
+
+function NetflixMovie1() {
+  return (
+    <>
+      <NavbarComp/>
+    </>
+  )
+}
+
+export default NetflixMovie1
