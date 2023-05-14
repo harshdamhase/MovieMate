74cc194fe129b963696aeba72fd9b513d53cbcb6
Mr-Siddhant-Bagul <siddhantbagul16@gmail.com>
2023-05-14 23:22:25 +0530

    Poster Added

--- src/Languages/EnglishMovie/English.js
+++ src/Languages/EnglishMovie/English.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-
-import React from 'react'
-import App from '../../App';
-import './English.css'
-
-function English() {
-  return (
-    <div>
-       <h1>Search "English Movie"</h1>
-        </div>
-  )
-}
-
-export default English;
-=======
 import React from 'react'
 
 function English() {
@@ -23,4 +7,3 @@ function English() {
 }
 
 export default English
->>>>>>> fff87770c6c067f6429ee2b78996f882acb9e872
--- src/Ott/Netflix/NetflixMovie1.js
+++ src/Ott/Netflix/NetflixMovie1.js
@@ -1,12 +1,37 @@
 import React from 'react'
+import "./Netflix.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Netflix.css"
 import NavbarComp from '../../Components/NavbarComp'
+import MovieReviewCard from '../../Components/MovieReviewCard';
+
 
 function NetflixMovie1() {
   return (
     <>
       <NavbarComp/>
+
+      <div className='Headline mt-5'>
+        <font className='PosterTitle' >
+            Squid Game
+        </font>
+      </div>
+
+      <div>
+        <MovieReviewCard ImgUrl="https://c4.wallpaperflare.com/wallpaper/529/888/855/squid-game-tv-hd-wallpaper-preview.jpg" />
+      </div>
+      <div className='row mt-5'>
+        <div className='col-lg-9'>
+          <img src='https://www.pinkvilla.com/images/2022-09/squid_game_poster_main.jpg?width=752&t=pvorg' className='Poster ms-5'/>
+          <span>
+            
+            <br/>
+            <font>
+              
+            </font>
+          </span>
+        </div>
+      </div>
     </>
   )
 }
