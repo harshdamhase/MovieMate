e3148971b804d32b38ece2e5e5cd569147a27876
Harshda Mhase <harshadamhase12@gmail.com>
2023-05-13 21:45:50 -0900

    English movie component added

---
+++ src/Languages/EnglishMovie/English.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import App from '../../App';
+import './English.css'
+
+function English() {
+  return (
+    <div>
+       <h1>Search "English Movie"</h1>
+        </div>
+  )
+}
+
+export default English;
