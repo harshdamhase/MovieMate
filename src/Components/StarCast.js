1e195e3bec7fe28bfa00328f8a540e2b805f2ec0
Mr-Siddhant-Bagul <siddhantbagul16@gmail.com>
2023-05-16 01:45:50 +0530

    Modify Cards

--- src/Components/StarCast.js
+++ src/Components/StarCast.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function StarCast({imgURl,StarName,StarMovieName,Episode}) {
   return (
     <>
-    <div class="cards mb-1 col-lg-3 ms-5" >
+    <div class="card Starcast mb-1 col-lg-3 ms-5" >
         <div class="row g-0">
             <div class="col-md-4">
             <img src={imgURl} class="img-fluid imgRound rounded-start" alt="..."/>
