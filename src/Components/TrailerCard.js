aed9d2228a02cf02ac1c3f0a7c2bb0c44cefc773
neha pawar <itsnehu13@gmail.com>
2023-05-15 23:04:41 +0530

    trailer link added

--- src/Components/TrailerCard.js
+++ src/Components/TrailerCard.js
@@ -2,15 +2,15 @@ import React from 'react';
 import "./Navbar.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function TrailerCard({ImgUrl,Rating, Starimg, title, WatchTriler }) {
+function TrailerCard({ImgUrl,Rating, Starimg, title,link, WatchTriler }) {
   return (
     <>
-        <div class="cards">
-            <img src={ImgUrl} class="card-img-top" alt="..."/>
+        <div class= "cards">
+            <img src={ImgUrl} class="card-img-top mt-2" alt="..."/>
             <div class="card-body">
             <p class="card-text">{Rating} <img src={Starimg} class="Starimg" alt="..."/></p>
             <h5 class="card-title">{title}</h5>
-            <a href="#" class="btn btn-primary">{WatchTriler}</a>
+            <a href={link} class= "btn btn-primary">{WatchTriler}</a>
             </div>
         </div>
     </>
--- src/Hompage.js
+++ src/Hompage.js
@@ -20,7 +20,7 @@ function Hompage() {
 
           <div className=' col-lg-4 col-sm-8 col-md-8 ms-5 info '>
             <img className="img-fluid elementalimg " src="https://mickeyblog.com/wp-content/uploads/2023/04/Elemental-Main-720x340.jpg"/>
-            <font className="Desc">Upcoming Movie </font>
+            <font className="also-like">Upcoming Movie</font>
             <div >
               <span className='title-style'>Release:</span><span className='inside'>Date:16 June 2023 (India)</span>
             </div>
@@ -55,35 +55,35 @@ function Hompage() {
         </div>
       </div> 
 
-    <div className='row trailer-name col-lg-12 ms-5 ps-5 '>
-        <font className="Desc mb-3">
-          Adrak and Lahsan
+    <div className='row trailer-name col-lg-12 ms-5 ps-5 mt-5 '>
+        <font className="also-like mb-3">
+         More LIke This
         </font>
-        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="bchjakhejeb" WatchTriler="cqjkwebvhjfv" className="TrailerCard" />
+        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="Mario Brows" link="https://youtu.be/TnGl01FkMMo" WatchTriler="Watch Traile" className="TrailerCard" />
 
-        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="bchjakhejeb" WatchTriler="cqjkwebvhjfv" className="TrailerCard" />
+        <TrailerCard ImgUrl="https://m.media-amazon.com/images/M/MV5BMGIxMmJjZDctOGQwOC00OTNkLWJiNmItN2M3M2M2NmExYWQ1XkEyXkFqcGdeQXVyNjkwOTg4MTA@._V1_.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="Fulrani" link="https://youtu.be/p-au4yC6OhY" WatchTriler="Watch Trailer" className="TrailerCard" />
 
-        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="bchjakhejeb" WatchTriler="cqjkwebvhjfv" className="TrailerCard" />
+        <TrailerCard ImgUrl="https://assetscdn1.paytm.com/images/cinema/adipurush2-min-c8528ea0-e671-11ed-82e8-cf24b6293ec4.jpg" Rating="6.0/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="Adipurush" link="https://youtu.be/scNmYjoR-qM" WatchTriler="Watch Trailrt" className="TrailerCard" />
 
-        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="bchjakhejeb" WatchTriler="cqjkwebvhjfv" className="TrailerCard" />
+        <TrailerCard ImgUrl="https://static.toiimg.com/thumb/msid-96573171,width-219,height-317,imgsize-19520/96573171.jpg" Rating="8.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="Ps-2" link="" WatchTriler="Watch Traile" className="TrailerCard" />
 
-        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="bchjakhejeb" WatchTriler="cqjkwebvhjfv" className="TrailerCard" />
+        <TrailerCard ImgUrl="https://lumiere-a.akamaihd.net/v1/images/p_thelittlemermaid_2023_final_796_94759fcc.jpeg?region=0%2C0%2C540%2C810" Rating="7.1/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="Little Mermaid" link="https://youtu.be/kpGo2_d3oYE" WatchTriler="Watch Traile" className="TrailerCard" />
 
-        <TrailerCard ImgUrl="https://www.amadorvalleytoday.org/wp-content/uploads/2023/04/MV5BOTJhNzlmNzctNTU5Yy00N2YwLThhMjQtZDM0YjEzN2Y0ZjNhXkEyXkFqcGdeQXVyMTEwMTQ4MzU5._V1_FMjpg_UX1000_-609x900.jpg" Rating="7.8/10" Starimg="https://cdn-icons-png.flaticon.com/128/9796/9796698.png" title="bchjakhejeb" WatchTriler="cqjkwebvhjfv" className="TrailerCard" />
+        
     </div>
 
     
 
     
-    <div className='mt-5 ms-5 '>
-      <div className='also-like '>
-        You May Also Like 
+    <div className=' row mt-5 ms-5 ps-5 col-lg-12 '>
+      <div className='also-likee mb-3 '>
+        Recommended Movies  <span className='see-More'><a href=""> See More</a></span>
       </div>
       <div className='poster-container'>
         <div className='img-box'>
           <img className='poster-img m-2 ' src='https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS8xMCAgMTQ5LjdLIHZvdGVz,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00343918-bcrtrqpgtc-portrait.jpg'/>
 
-          <img className='poster-img   m-2' src='https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS4zLzEwICAzNC40SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00310794-nmwukaluxy-portrait.jpg'/>
+          <img className='poster-img  m-2' src='https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OS4zLzEwICAzNC40SyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00310794-nmwukaluxy-portrait.jpg'/>
 
           <img className='poster-img  m-2' src='https://assets-in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:w-400.0,h-660.0,cm-pad_resize,bg-000000,fo-top:oi-discovery-catalog@@icons@@star-icon-202203010609.png,ox-24,oy-615,ow-29:ote-OC8xMCAgMS4ySyB2b3Rlcw%3D%3D,ots-29,otc-FFFFFF,oy-612,ox-70:q-80/et00357451-jgmzaupytp-portrait.jpg'/>
 
