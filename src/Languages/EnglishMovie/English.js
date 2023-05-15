552e0c04f336bb455db9737e87759c2c277d26c7
Harshda Mhase <harshadamhase12@gmail.com>
2023-05-14 21:58:09 -0900

    navbar add in english page

--- src/Languages/EnglishMovie/English.js
+++ src/Languages/EnglishMovie/English.js
@@ -1,26 +1,17 @@
-<<<<<<< HEAD
-
 import React from 'react'
-import App from '../../App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavbarComp from '../../Components/NavbarComp';
 import './English.css'
 
 function English() {
   return (
     <div>
-       <h1>Search "English Movie"</h1>
-        </div>
-  )
-}
-
-export default English;
-=======
-import React from 'react'
-
-function English() {
-  return (
-    <div>English</div>
+      <NavbarComp />
+      <div className='card'>
+        
+      </div>
+    </div>
   )
 }
 
 export default English
->>>>>>> fff87770c6c067f6429ee2b78996f882acb9e872
