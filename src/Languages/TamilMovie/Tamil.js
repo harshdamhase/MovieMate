95fc45649becff544af62670e72910a4929e5af8
Mr-Siddhant-Bagul <siddhantbagul16@gmail.com>
2023-05-18 00:28:44 +0530

    tamil

--- src/Languages/TamilMovie/Tamil.js
+++ src/Languages/TamilMovie/Tamil.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import NavbarComp from '../../Components/NavbarComp'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img1 from '../TamilMovie/Images/Hit Tamil.jpg'
+import img2 from "../TamilMovie/Images/lidsa.jpg"
+import img3 from "../TamilMovie/Images/patro.webp"
+import TamilCarousel from '../../Components/TamilCarousel'
+import "./Tamil.css"
+
+
+
 
 function Tamil() {
   return (
-    <div>Tamil</div>
+    <>
+      <NavbarComp/>
+      <div className='TamilCarousel'>
+      <TamilCarousel img1={img1} img2={img2} img3={img3}/>
+      </div>
+      <hr/>
+      <div>
+        <p className=''>
+          Popular Tollywood Movies:
+        </p>
+      </div>
+
+
+    </>
   )
 }
 
