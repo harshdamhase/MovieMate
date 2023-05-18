4d1ed6728e6da1e507945d95b69d637a5bc75ca6
Pawan Raj <pawanraj85074@gmail.com>
2023-05-18 13:49:49 +0530

    horror card added

--- src/Languages/HindiMovie/Hindi.js
+++ src/Languages/HindiMovie/Hindi.js
@@ -14,8 +14,10 @@ function Hindi() {
     
     <div>
       <NavbarComp/>
+      
+           
     <div>
-      <Carousel>
+    <Carousel>
       <Carousel.Item>
         <img  style={{height:'350px'}}
           className="d-block w-100 carousel-container"
@@ -26,8 +28,8 @@ function Hindi() {
           <h3>Latest Thriller Movies</h3>
         </Carousel.Caption>
       </Carousel.Item>
-      <Carousel.Item>
-        <img style={{height:'350px'}}
+    <Carousel.Item>
+       <img style={{height:'350px'}}
           className="d-block w-100 carousel-container"
           src={img2}
           alt="Second slide"
@@ -51,7 +53,7 @@ function Hindi() {
     </Carousel>
     </div>
     <section class="features" >
-       <h2 className="my-3" style={{color:'goldenrod',marginLeft:'90px',marginBottom:'10px'}}>Most Popular</h2>
+       <h2 className="my-3 " style={{color:'goldenrod',marginLeft:'80px',marginBottom:'10px',backgroundColor:'#737373',width:'88%',height:'45px'}}><span style={{color:'goldenrod',marginLeft:'10px'}}>Most Popular</span></h2>
         <div className="row  mx-3 px-5" style={{marginBottom:'10px',}}>
         <div class="col-md-3" >
             <HindiMovieCard imgUrl="https://dotmovies.bio/wp-content/uploads/2023/05/The-Kerala-Story.jpg" filmName="The Kerala Story" timeDuration="1hrs 35mins" filmType="Action"/>
@@ -84,6 +86,39 @@ function Hindi() {
             </div>
             </section>
 
+            <section class="features" >
+       <h2 className="my-3 " style={{marginLeft:'80px',marginBottom:'10px',backgroundColor:'#737373',width:'88%',height:'45px'}}><span style={{color:'goldenrod',marginLeft:'10px'}}>Horror Movies</span></h2>
+        <div className="row  mx-3 px-5" style={{marginBottom:'10px',}}>
+        <div class="col-md-3" >
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BZTZlNmM3MmQtM2U5NS00MTkzLWI5MGYtYTQ3OWM0OGUwYTk3XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR2,0,140,209_AL_.jpg" filmName="Naina" timeDuration="1hrs 35mins" filmType="Horror"/>
+            </div>
+            <div class="col-md-3">
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BM2JkYTJjODAtZjYwZS00NTU0LTg1YjAtYzY4YTYxN2VkMTk3XkEyXkFqcGdeQXVyMzc0NzU5MTc@._V1_UY209_CR2,0,140,209_AL_.jpg" filmName=" Raaz" timeDuration="1hrs 30mins" filmType="Horror"/>
+            </div>
+            <div class="col-md-3">
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BODk1NzZiNTktZmIzMi00NzM1LTllMGEtOTg1NWE4NTUwZmIxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR3,0,140,209_AL_.jpg" filmName=" Ragini MMS" timeDuration="1hrs 55mins" filmType="Horror"/>
+            </div>
+            <div class="col-md-3">
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BZWE3Nzc1MWMtMTczZS00M2Q4LTgyYTMtODE2MzY2NTkzZTA3XkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR3,0,140,209_AL_.jpg" filmName="Ragini MMS 2" timeDuration="1hrs 40mins" filmType="Horror"/>
+            </div>
+
+
+            </div>
+            <div className="row  mx-3 px-5" style={{marginBottom:'10px',}}>
+        <div class="col-md-3" >
+        <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BNjQ2N2QzYzktZGNjYy00ZTUyLWE2ZjYtMTEzZGY4YzIwNTJlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR3,0,140,209_AL_.jpg" filmName="1920 " timeDuration="1hrs 40mins" filmType="Horror"/>
+            </div>
+            <div class="col-md-3">
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BYzg3ZjRlYjgtOGJkNy00MThkLThhY2UtMDdhMDZhMTc2NDFlXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_UY209_CR3,0,140,209_AL_.jpg" filmName="Raaz 3" timeDuration="1hrs 55mins" filmType="Horror"/>
+            </div>
+            <div class="col-md-3">
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BNjEwYTBlZjktYjczZS00ZGQ0LWJlNWItMDE1YjMzNmFhNWIxXkEyXkFqcGdeQXVyNDMxMjY3NDA@._V1_UY209_CR69,0,140,209_AL_.jpg" filmName="1920: Evil Returns" timeDuration="1hrs 40mins" filmType="Horror"/>
+            </div>
+            <div class="col-md-3">
+            <HindiMovieCard imgUrl="https://m.media-amazon.com/images/M/MV5BYzYwYjg1NTctODRlNC00YzQ3LWJjNmItMWI4ODkyOGY3ZWZiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_UY209_CR2,0,140,209_AL_.jpg" filmName="Aatma" timeDuration="1hrs 30mins" filmType="Horror"/>
+            </div>
+            </div>
+            </section>
 
       </div>
   )
