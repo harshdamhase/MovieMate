56868d8934b2e2d68a0bb603b59538bde78623e3
neha pawar <itsnehu13@gmail.com>
2023-05-19 01:02:59 +0530

    form

--- src/Other/TicketBooking/Ticket.js
+++ src/Other/TicketBooking/Ticket.js
@@ -14,13 +14,25 @@ function Ticket(){
 
         <div className='container-form'>
             <h1>Book your Tickits| Enjoy your day</h1>
-           <label>
-            name
-           <input type="text" value={name} onChange ={(e) => 
-              setName(e.target.value)
-            }/>
-            </label>
-            <br/><br/>
+            
+
+                <div class="form-floating mb-3">
+               
+                  <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com"
+                   value={name} onChange ={(e) => 
+                      setName(e.target.value)}/>
+                  <label for="floatingInput">Email address</label>
+                   
+                 <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                    <input type="email"  class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" 
+                     value={name} onChange ={(e) => 
+                      setName(e.target.value)}/>  
+
+                    </div>
+                    <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
            
             <button onClick={submit} type ="button">submit </button>
             <br/><br/>
