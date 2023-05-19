2c8a93b40cfe1718f2d5c360da13d65404c07205
avinashwele <60333815+avinashwele@users.noreply.github.com>
2023-05-19 19:26:55 +0530

    storyline add

--- src/Ott/Amazon/AmazonMovie2.js
+++ src/Ott/Amazon/AmazonMovie2.js
@@ -22,20 +22,20 @@ function AmazonMovie2() {
           </font>
         </div>
         <div>
-          <MovieReviewCard ImgUrl="https://www.google.com/url?sa=i&url=https%3A%2F%2Fgamingtrend.com%2Fnews%2Fstuck-at-home-sonic-the-hedgehog-movie-gets-an-early-home-video-release-this-month%2F&psig=AOvVaw17CNf54Twz6EevU_rAAGGr&ust=1684585710692000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCNiepvmwgf8CFQAAAAAdAAAAABAE" />
+          <MovieReviewCard ImgUrl="https://legendary-digital-network-assets.s3.amazonaws.com/wp-content/uploads/2020/03/13050006/lrDyljrQ.jpeg" />
         </div>
       </div>
 
       <div className='row mt-2'>
         <div className='row PostDesc p-3'>
-          <img src='https://m.media-amazon.com/images/M/MV5BZjZkY2YxYTgtYmVkOC00NTgwLThjY2YtN2Y1ZWFlZmVhOTQ5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg' alt="img" className='Poster ms-5 col-lg-3 col-md-3 col-sm-3 col-xs-3'/>
+          <img src='https://www.themoviedb.org/t/p/original/a1KFPJtTmT5VlCvANsTQF7IA5y.jpg' alt="img" className='Poster ms-5 col-lg-3 col-md-3 col-sm-3 col-xs-3'/>
           <font className='DescTitle col-lg-8 col-md-8 col-sm-8 col-xs-8 ms-3' >
             <h1 className='ms-2 PosterName'>
             Sonic The Hedgehog
             </h1>
             <br/><br/>
             <span className='ms-5'>
-              <SeasonBtn SeasonNum="Season 1"/>
+              <SeasonBtn SeasonNum="Sonic Part-1"/>
             </span>
             <h5 className='Desc mt-3 ms-5'>
               <font className='ReleaseDateFont'>Release Date: 25 January 2020</font>
@@ -48,9 +48,6 @@ function AmazonMovie2() {
             <div className='Genre col-lg-8 col-md-9 col-sm-9 col-xs-9 mt-5 '>
               <span className='ms-1 mb-3'>
                 <SeasonBtn SeasonNum="Action"/>
-              </span>
-              <span className='ms-1'>
-                <SeasonBtn SeasonNum="Drama"/>
               </span>
               <span className='ms-1'>
                 <SeasonBtn SeasonNum="Comedy"/>
@@ -84,17 +81,17 @@ function AmazonMovie2() {
           Star Cast:
         </font>
         <div className='row StarCastDiv'>
-          <AmazonStarCasts imgURl="https://www.imdb.com/name/nm2355635/?ref_=tt_cl_i_1" StarName={<a href='https://www.imdb.com/name/nm2355635/?ref_=tt_cl_t_1' className='StarCastText'>Ben Schwartz</a>} StarMovieName="Sonic (Voice)"  className="Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
+          <AmazonStarCasts imgURl="#" StarName={<a href='#' className='StarCastText'>Ben Schwartz</a>} StarMovieName="Sonic (Voice)"  className="Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
 
-          <AmazonStarCasts imgURl="https://www.imdb.com/name/nm0005188/?ref_=tt_cl_i_2" StarName={<a href='https://www.imdb.com/name/nm0005188/?ref_=tt_cl_t_2' className='StarCastText' >James Marsden</a>} StarMovieName="Tom" className="col-lg-3 col-md-9 col-sm-9 col-xs-9" />
+          <AmazonStarCasts imgURl="#" StarName={<a href='#' className='StarCastText' >James Marsden</a>} StarMovieName="Tom" className="col-lg-3 col-md-9 col-sm-9 col-xs-9" />
 
-          <AmazonStarCasts imgURl="https://www.imdb.com/name/nm0000120/?ref_=tt_cl_i_3" StarName={<a href='https://www.imdb.com/name/nm0000120/?ref_=tt_cl_t_3' className='StarCastText'>Jim Carrey</a>} StarMovieName="Dr. Robotnik"  className="Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
+          <AmazonStarCasts imgURl="#" StarName={<a href='#' className='StarCastText'>Jim Carrey</a>} StarMovieName="Dr. Robotnik"  className="Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
 
-          <AmazonStarCasts imgURl="https://www.imdb.com/name/nm1754366/?ref_=tt_cl_i_4" StarName={<a href='https://www.imdb.com/name/nm1754366/?ref_=tt_cl_t_4' className='StarCastText' >Tika Sumpter</a>} StarMovieName="Maddie"  className=" Col-lg-3 col-md-6 col-sm-9 col-xs-9" />
+          <AmazonStarCasts imgURl="#" StarName={<a href='#' className='StarCastText' >Tika Sumpter</a>} StarMovieName="Maddie"  className=" Col-lg-3 col-md-6 col-sm-9 col-xs-9" />
 
-          <AmazonStarCasts imgURl="https://www.imdb.com/name/nm5625020/?ref_=tt_cl_i_5" StarName={<a href='https://www.imdb.com/name/nm5625020/?ref_=tt_cl_t_5' className='StarCastText'>Natasha Rothwell</a>} StarMovieName="Rachel"  className=" Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
+          <AmazonStarCasts imgURl="#" StarName={<a href='#' className='StarCastText'>Natasha Rothwell</a>} StarMovieName="Rachel"  className=" Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
 
-          <AmazonStarCasts imgURl="https://www.imdb.com/name/nm1269723/?ref_=tt_cl_i_6" StarName={<a href='https://www.imdb.com/name/nm1269723/?ref_=tt_cl_t_6' className='StarCastText'>Adam Polly</a>} StarMovieName="Wade"  className=" Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
+          <AmazonStarCasts imgURl="#" StarName={<a href='#' className='StarCastText'>Adam Polly</a>} StarMovieName="Wade"  className=" Col-lg-3 col-md-9 col-sm-9 col-xs-9" />
         </div>
       </div>
 
@@ -102,18 +99,18 @@ function AmazonMovie2() {
 
       <div className='row EpisodeCardDiv ms-5'>
       <font className="CastName ms-5 mt-2 ps-5">
-          10 EPISODES
+          Movie Series list
         </font>
         <div >
-          <EpisodeCard EpisodeName="Movie Series" EpisodeKeyword="Sonic the Hedgehog (2020) " EpisodeContent="Powered with incredible speed, Sonic The Hedgehog embraces his new home on Earth. That is, until Sonic sparks the attention of super-uncool evil genius Dr. Robotnik. Now it's super-villain vs. super-sonic in an all-out race across the globe to stop Robotnik from using Sonic's unique power for world domination." />
+          <EpisodeCard EpisodeName="Part-1" EpisodeKeyword="Sonic the Hedgehog (2020) " EpisodeContent="Powered with incredible speed, Sonic The Hedgehog embraces his new home on Earth. That is, until Sonic sparks the attention of super-uncool evil genius Dr. Robotnik. Now it's super-villain vs. super-sonic in an all-out race across the globe to stop Robotnik from using Sonic's unique power for world domination." />
         </div>
 
         <div >
-        <EpisodeCard EpisodeName="Movie Series"   EpisodeKeyword="Sonic the Hedgehog 2(2022)" EpisodeContent="After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands." />
+        <EpisodeCard EpisodeName="Part-2" EpisodeKeyword="Sonic the Hedgehog 2(2022)" EpisodeContent="After settling in Green Hills, Sonic is eager to prove he has what it takes to be a true hero. His test comes when Dr. Robotnik returns, this time with a new partner, Knuckles, in search for an emerald that has the power to destroy civilizations. Sonic teams up with his own sidekick, Tails, and together they embark on a globe-trotting journey to find the emerald before it falls into the wrong hands." />
         </div>
 
         <div>
-        <EpisodeCard EpisodeName="Movie Series"   EpisodeKeyword="Sonic the Hedgehog 3 (2024) [Upcoming....]" EpisodeContent="After defeating Robotnik, Sonic discovers that a project named “Project Shadow” has escaped from the military and started wrecking havoc in town, Sonic, Tails and Knuckles team up to defeat Shadow, Shadow desires to honor the legacy of Maria and demands the location of the legendary “Fleetway Emerald,, an ancient relic that allows its possessor to unleash ultimate power." />
+        <EpisodeCard EpisodeName="Part-3" EpisodeKeyword="Sonic the Hedgehog 3 (2024) [Upcoming....]" EpisodeContent="After defeating Robotnik, Sonic discovers that a project named “Project Shadow” has escaped from the military and started wrecking havoc in town, Sonic, Tails and Knuckles team up to defeat Shadow, Shadow desires to honor the legacy of Maria and demands the location of the legendary “Fleetway Emerald,, an ancient relic that allows its possessor to unleash ultimate power." />
         </div>
 
       </div>
@@ -150,7 +147,7 @@ function AmazonMovie2() {
         <DetailsNetflix DetailsInfo="Directed by	:" Details="Jeff Fowler"/>
         <DetailsNetflix DetailsInfo="Written by :" Details="Pat Casey  Josh Miller"/>
         <DetailsNetflix DetailsInfo="Runtime :" Details="1H 40Min"/>
-        <DetailsNetflix DetailsInfo="Languages :" Details="English Hindi"/>
+        <DetailsNetflix DetailsInfo="Languages :" Details="English"/>
         <DetailsNetflix DetailsInfo="Original release :" Details="25 January 2020"/>
       </div>
 
