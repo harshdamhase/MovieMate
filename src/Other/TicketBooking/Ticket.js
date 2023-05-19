b2eeafba5c6a9f34b13a45c683be272ffb1a70a7
neha pawar <itsnehu13@gmail.com>
2023-05-19 15:04:03 +0530

    form

--- src/Other/TicketBooking/Ticket.js
+++ src/Other/TicketBooking/Ticket.js
@@ -9,12 +9,15 @@ function Ticket(){
     }
     
     const[name,setName]= useState('')
+    const[email,setEmail]= useState('')
+    const[city,setcity]= useState('')
+
     return(
         <>
     
-    <div className='container'>
+    <div className='container TicketDiv'>
         <div className='container-form'>
-            <h2>Book your Tickits| Enjoy your day</h2>
+            <h4 className='heading'>Book your Tickits| Enjoy your day</h4>
             
 
                 <div className='form-floating form-box mt-4 mb-1 me-10'>
@@ -25,14 +28,14 @@ function Ticket(){
                      
                  <div className='form-floating mb-1 '>
                     <input type="email"  class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" 
-                     value={name} onChange ={(e) => setName(e.target.value)}/> 
+                     value={email} onChange ={(e) => setEmail(e.target.value)}/> 
                       <label for="exampleFormControlInput1" class="form-label">Email Address</label> 
 
                 </div>
 
                 <div className='form-floating mb-1'>
                     <input type="date"  class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" 
-                     value={name} onChange ={(e) =>setName(e.target.value)}/> 
+                     value={name} onChange ={(e) =>setcity(e.target.value)}/> 
                       <label for="exampleFormControlInput1" class="form-label">Select Time</label> 
 
                 </div>
@@ -40,14 +43,17 @@ function Ticket(){
             <div className='location-box'>
             <span class="col-md-6 form-floating left">
                  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" 
-                  value={name} onChange ={(e) => setName(e.target.value)}/> 
+                  value={city} onChange ={(e) => setName(e.target.value)}/> 
                 <label for="exampleFormControlInput1" class="form-label">Choose City</label>
             </span>
 
             <span class="col-md-3 right form-floating ">
                     <select class="form-select" id="" >
                     <option selected disabled value="">Location</option>
-                    <option>cineplex</option>
+                    <option>Cinepolis</option>
+                    <option>AM Cinema</option>
+                    <option>INOX</option>
+                    <option>PVR</option>
                     </select>
             </span>
         </div>
@@ -57,33 +63,40 @@ function Ticket(){
 
         <div className='radio-sec'>
 
-                <span className=''>Pyament Method:</span>
+                <span className=''>Payment Method:</span>
 
                 <span class="form-check form-check-inline margin">
-                <input class="form-check-input" type="radio" id="inlineCheckbox1" value="option1"></input>
+                <input class="form-check-input" type="radio"name="Payment" id="inlineCheckbox1" value="option1"></input>
                 
-                <label class="form-check-label" for="inlineCheckbox1">Paytm</label>
+                <label class="form-check-label" for="inlineCheckbox1">Cash</label>
                 </span>
                 <span class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" id="inlineCheckbox2" 
+                <input class="form-check-input" name="Payment" type="radio" id="inlineCheckbox2" 
                 value={name} onChange ={(e) => setName(e.target.value)}/> 
                 <label class="form-check-label" for="inlineCheckbox2">Googlepay</label>
                 </span>
                 <span class="form-check form-check-inline">
-                <input class="form-check-input" type="radio" id="inlineCheckbox3" 
+                <input class="form-check-input"  type="radio" name="Payment" id="inlineCheckbox3" 
                 value ={name} onChange ={(e) => setName(e.target.value)}/> 
                 <label class="form-check-label" for="inlineCheckbox3">Phonepay</label>
+                </span>
+
+                <span class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="Payment" id="inlineCheckbox3" 
+                value ={name} onChange ={(e) => setName(e.target.value)}/> 
+                <label class="form-check-label" for="inlineCheckbox3">Paytm</label>
                 </span>
 </div>
     
        
 
     
-            
+                    
+
            
         <div className='b-section '>
             <span class="col-auto left">
-              <button onClick={submit} type="submit" className='btn btn-primary  mb-3' >Book your Ticket</button>
+              <button onClick={submit} type="submit" className='btn btn-primary form-bttn  mb-3' >Book your Ticket</button>
             </span>
         
 
